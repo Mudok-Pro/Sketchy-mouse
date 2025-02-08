a517d4ea784cf13182b384921260a41c44763889
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-08 14:52:05 +0100

    Add getRandomColor function and dynamically set square width and height for perfect squares

--- javascript.js
+++ javascript.js
@@ -2,14 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
     function createGrid(size) {
         const container = document.getElementById('container');
         container.innerHTML = '';
+        const squareSize = 100 /size ;
         for (let i = 0; i < size * size; i++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            square.style.width = `${squareSize}%`
+            square.style.height = `${squareSize}%`
             square.addEventListener('mouseover', () => {
-                square.style.backgroundColor = 'black';
+                square.style.backgroundColor = getRandomColor();
             });
+
+            square.addEventListener('mouseover', () => {
+                let currentOpacity = parseFloat(else.target.style.opacity) || 0 ;
+                if (currentOpacity <1) {
+                    e.target.style.opacity = currentOpacity + 0.1 ;
+                }
+            })
             container.appendChild(square);
         }
+    }
+    function getRandomColor (){
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`; 
     }
 
     document.getElementById('reset').addEventListener('click', () => {
